4d4399c65e9966db78dd4e1228f9a60a923b77e0
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-05-25 07:29:36 +0900

    Add project files.

---
+++ ClassNameSpike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassNameSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
