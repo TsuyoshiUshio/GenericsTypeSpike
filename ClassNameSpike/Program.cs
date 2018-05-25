b3cffd19aacdc61e56aa3af9ac2b8c3485c056c8
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-05-25 13:23:27 +0900

    initial commit

--- ClassNameSpike/Program.cs
+++ ClassNameSpike/Program.cs
@@ -2,11 +2,61 @@
 
 namespace ClassNameSpike
 {
+    public interface IClass
+    {
+        string Name { get; set; }
+    }
+
+    public abstract class BaseSomeClass : IClass
+    {
+        public string Name { get; set; }
+        public abstract string Hello();
+    }
+
+    public class SomeClass : BaseSomeClass
+    {
+        public override string Hello()
+        {
+            return "Hello from SomeClass";
+        }
+    }
+    public class GenericSome
+    {
+        public string HelloByNameOf<T>()
+        {
+            return $"Hello! The parameter is {nameof(T)}";
+        }
+        public string HelloByTypeOf<T>()
+        {
+            return $"Hello! The parameter is {typeof(T)}";
+        }
+
+        public string HelloByGetType<T>()
+        {
+            var type = typeof(T);
+            return $"Hello! The parameter is {type.GetType()}";
+
+        }
+
+        public string HelloByNameAndNamespace<T>()
+        {
+            var type = typeof(T);
+            return $"Hello! The parameter is Name: {type.Name} Namespace: {type.Namespace}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"nameof(\"SomeClass\"):" +
+                $"{nameof(SomeClass)}");
+            var gsome = new GenericSome();
+            Console.WriteLine($"Generic method nameof: {gsome.HelloByNameOf<SomeClass>()}");
+            Console.WriteLine($"Generic method typeof: {gsome.HelloByTypeOf<SomeClass>()}");
+            Console.WriteLine($"Generic method GetType: {gsome.HelloByGetType<SomeClass>()}");
+            Console.WriteLine($"Generic method GetType: {gsome.HelloByNameAndNamespace<SomeClass>()}");
+
+            Console.ReadLine();
         }
     }
 }
